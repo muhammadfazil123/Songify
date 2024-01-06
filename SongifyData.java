64a070ce328ad30fb8b48a62bde75829d1d1f068
muhammadfazil123 <22233584@bucks.ac.uk>
2024-01-06 14:39:39 +0000

    Variables added for the songify data class and a constructor method was used with the variables in the parameter so they can be initalised

---
+++ SongifyData.java
@@ -0,0 +1,15 @@
+public class SongifyData {
+
+    public String songTitle;
+    public String artistName;
+    public int streamCount;
+    public String genre;
+
+    public SongifyData(String songTitle, String artistName, int streamCount, String genre) {
+        this.songTitle = songTitle;
+        this.artistName = artistName;
+        this.streamCount = streamCount;
+        this.genre = genre;
+    }
+
+}
