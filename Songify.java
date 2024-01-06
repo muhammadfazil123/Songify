03fc58a1b624aa85a919b500e0cea27378a2188b
muhammadfazil123 <22233584@bucks.ac.uk>
2024-01-06 15:54:30 +0000

    Created an ArrayList object called songifyDatas that will store the information from the SongifyData class. Used the Songify constructor to add several songs to the ArrayList

---
+++ Songify.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Songify {
+
+    public ArrayList<SongifyData> songifyDatas = new ArrayList<SongifyData>();
+    
+    public Songify() {
+
+        songifyDatas.add(new SongifyData("Shippuden", "Blanco", 44000000, "Rap"));
+        songifyDatas.add(new SongifyData("Outside", "D-Block Europe", 267000000 , "Rap"));
+        songifyDatas.add(new SongifyData("Alone With You", "Arz", 177000000, "Rap"));
+        songifyDatas.add(new SongifyData("Savage Anthem", "PARTYNEXTDOOR", 150000000 , "R&B"));
+        songifyDatas.add(new SongifyData("Who Hurt You?", "Daniel Caesar", 762000000, "R&B"));
+        songifyDatas.add(new SongifyData("No Guidance", "Chris Brown", 468000000, "R&B"));
+        songifyDatas.add(new SongifyData("On The Low", "Burna Boy", 365000000, "Afrobeats"));
+        songifyDatas.add(new SongifyData("Soundgasm", "Rema", 670000000 , "Afrobeats"));
+        songifyDatas.add(new SongifyData("Soso", "Omah Lay", 123000000, "Afrobeats"));
+        songifyDatas.add(new SongifyData("Umbrella", "Rihanna", 988000000, "Hip-Hop"));
+        songifyDatas.add(new SongifyData("Only", "Nicki Minaj", 535000000, "Hip-Hop"));
+        songifyDatas.add(new SongifyData("Thang For You", "Rylo Rodrigeuz", 60000000 , "Hip-Hop"));
+       
+    }
+}
