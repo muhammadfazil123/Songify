2283967728eef489c9e98b3a19928d6c995d926e
muhammadfazil123 <22233584@bucks.ac.uk>
2024-01-06 16:14:08 +0000

    Changed variables from public to private to improve code quality and make sure there are no future errors. Getter and setter methods used so these private variables can be accessed

--- SongifyData.java
+++ SongifyData.java
@@ -1,14 +1,49 @@
 public class SongifyData {
 
-    public String songTitle;
-    public String artistName;
-    public int streamCount;
-    public String genre;
+    //private variables to improve code quality
+    private String songTitle;
+    private String artistName;
+    private int streamCount;
+    private String genre;
 
+    //constructor
     public SongifyData(String songTitle, String artistName, int streamCount, String genre) {
         this.songTitle = songTitle;
         this.artistName = artistName;
         this.streamCount = streamCount;
+        this.genre = genre;
+    }
+    
+    //getter methods
+    public String getsongTitle() {
+        return songTitle;
+    }
+
+    public String getartistName() {
+        return artistName;
+    }
+
+    public int getstreamCount() {
+        return streamCount;
+    }
+    public String getgenre() {
+        return genre;
+    }
+
+    //setter methods
+    public void setsongTitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+
+    public void setartistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public void setstreamCount(int streamCount) {
+        this.streamCount = streamCount;
+    }
+
+    public void setgenre(String genre) {
         this.genre = genre;
     }
 
